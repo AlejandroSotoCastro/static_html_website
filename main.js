1db3ca4b3249aaece9ac4c847d0a578136b187d5
AlejandroSotoCastro <alejandrosc.eng@gmail.com>
2021-04-22 14:39:13 +0200

    added sunglasses

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ const btns = document.querySelectorAll('button');
 // Convert buttons NodeList to an array
 const btnsArr = Array.from(btns);
 console.log(btnsArr)
+
+
+const aButton = document.querySelector('button');
+console.log(Array.from(aButton))
+let toggle= false;
+
+
+function dosomething(){
+    if (toggle){
+
+        console.log("this should do something")
+        aButton.style.backgroundColor="red"
+        
+        
+    }
+    else {aButton.style.backgroundColor="blue"}
+
+    toggle=!toggle
+
+}
